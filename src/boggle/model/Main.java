7b20254cc2ff02d32d3422d86a475cd9736df816
antunes26u <alexandre.antunes4@etu.univ-lorraine.fr>
2022-03-01 11:26:55 +0100

    ECriture d'une classe MenuVue (en cours)

--- src/boggle/model/Main.java
+++ src/boggle/model/Main.java
@@ -4,43 +4,20 @@ import boggle.view.PanneauControle;
 import boggle.view.VueInfos;
 import boggle.view.VueLettres;
 import boggle.view.VueMots;
+import boggle.view.menu.VueMenu;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
     @Override
     public void start (Stage primaryStage) {
-        // Create MenuBar
-        MenuBar menuBar = new MenuBar();
-
-        // Create menus
-        Menu fileMenu = new Menu("File");
-        Menu editMenu = new Menu("Edit");
-        Menu helpMenu = new Menu("Help");
-
-        // Create MenuItems
-        MenuItem newItem = new MenuItem("New");
-        MenuItem openFileItem = new MenuItem("Open File");
-        MenuItem exitItem = new MenuItem("Exit");
-
-        MenuItem copyItem = new MenuItem("Copy");
-        MenuItem pasteItem = new MenuItem("Paste");
-
-        // Add menuItems to the Menus
-        fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
-        editMenu.getItems().addAll(copyItem, pasteItem);
-
-        // Add Menus to the MenuBar
-        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
         Boggle bog = new Boggle(4) ;
+        VueMenu menu = new VueMenu(bog);
         bog.setDernierMot();
         BorderPane root = new BorderPane() ;
-        root.setTop(menuBar);
+        root.setTop(menu.getMenu());
         root.setBottom(new VueInfos(bog));
         root.setCenter(new VueLettres(bog));
         root.setRight(new PanneauControle(bog));
--- src/boggle/view/menu/VueMenu.java
+++ src/boggle/view/menu/VueMenu.java
@@ -1,10 +1,46 @@
 package boggle.view.menu;
 
+import boggle.Observateur;
 import boggle.model.Boggle;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 
-public class VueMenu extends VBox {
+public class VueMenu extends VBox implements Observateur {
+    private Boggle bog ;
+    private MenuBar menuBar;
     public VueMenu(Boggle bog){
+        super();
+        this.bog = bog;
+        // Create MenuBar
+        this.menuBar = new MenuBar();
+        // Create menus
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        Menu helpMenu = new Menu("Help");
+        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
+
+        // Create MenuItems
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openFileItem = new MenuItem("Open File");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        MenuItem copyItem = new MenuItem("Copy");
+        MenuItem pasteItem = new MenuItem("Paste");
+
+        // Add menuItems to the Menus
+        fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
+        editMenu.getItems().addAll(copyItem, pasteItem);
+        this.bog.ajouterObservateur(this);
+    }
+
+    public MenuBar getMenu (){
+        return this.menuBar;
+    }
+
+    @Override
+    public void reagir() {
 
     }
 }
